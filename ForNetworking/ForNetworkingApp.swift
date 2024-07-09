d88f9415c1d09522e80114faa3c8716d58eaf47b
Samandar <samandartoshpulatov0704@gmail.com>
2024-07-09 20:45:08 +0500

    Initial Commit

---
+++ ForNetworking/ForNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForNetworkingApp.swift
+//  ForNetworking
+//
+//  Created by Samandar on 09/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ForNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
